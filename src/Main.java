732923dedd0b1f47a7fd9f39425a6b2954718b43
somtech <oscarchisom11@gmail.com>
2022-07-10 21:38:30 +0100

    Main.java file

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        LowestCommonDivisor.lowestDivisor( 8,10 );
+        HigestCommonDivisior.highestDivisor( 12,24 );
+    }
+}
+
+// you can still use scanner to take the input directly from the user rather than using the static method call,
